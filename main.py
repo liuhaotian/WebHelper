9872a4630c2ded59c587f6d42d43cd558bb9ab2d
Haotian Liu <liuhaotian.air@gmail.com>
2013-04-28 14:08:22 -0700

    Connection pool and caching

--- main.py
+++ main.py
@@ -11,15 +11,18 @@ from flask import Flask, redirect
 
 
 app = Flask(__name__)
+session = requests.session()
+session.keep_alive = False
+session.headers.update({'User-Agent': 'Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10'})
 
 
 @app.route("/instagram/<path:url>")
 def instagram(url):
     if not urlparse(url).scheme:
         url = 'http://' + url
-    resp = requests.get(url)
+    resp = session.get(url)
     image_url = re.search(r'http://[\w]*image[\d]*.ak.instagram.com/[\d\w_]*\.jpg', resp.content).group()
-    return redirect(image_url)
+    return redirect(image_url, 301)
 
 
 if __name__ == "__main__":
